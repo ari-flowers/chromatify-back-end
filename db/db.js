523a7f83a8ba35413a3ee71bd7dc653697a5282a
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2021-10-15 13:50:49 -0700

    heroku code PART TWOOOO

--- db/db.js
+++ db/db.js
@@ -1,16 +1,22 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const connection = "mongodb+srv://chromaUser:xe13PWbS6uPrADCM@cluster0.f5n4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+
 const DB_URL = process.env.DB_URL || 'mongodb://localhost/chromatifys'
 
-// Error / Disconnect
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
-
-// Connection string
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}
-
-mongoose.connect(DB_URL, options)
-mongoose.connection.on('open', () => {console.log('connected to mongoose...')})
+// // Error / Disconnect
+// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+//
+// // Connection string
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }
+//
+// mongoose.connect(DB_URL, options)
+// mongoose.connection.on('open', () => {console.log('connected to mongoose...')})
